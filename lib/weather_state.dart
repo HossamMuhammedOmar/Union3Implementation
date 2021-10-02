525e75eb9f0aad1520bc62f336f7c371fb421955
Hossam Omar <hossamomar@Hossams-MacBook-Pro.local>
2021-10-02 12:36:30 +0200

    update example class

--- lib/weather_state.dart
+++ lib/weather_state.dart
@@ -2,24 +2,6 @@ import 'package:sealed_unions/factories/quartet_factory.dart';
 import 'package:sealed_unions/implementations/union_4_impl.dart';
 import 'package:sealed_unions/union_4.dart';
 
-// class WeatherState
-//     extends Union3Impl<_WeatherInitial, _WeatherLoading, _WeatherLoaded> {
-//   static const Triplet<_WeatherInitial, _WeatherLoading, _WeatherLoaded>
-//       _factory = Triplet<_WeatherInitial, _WeatherLoading, _WeatherLoaded>();
-
-//   WeatherState._(Union3<_WeatherInitial, _WeatherLoading, _WeatherLoaded> union)
-//       : super(union);
-
-//   factory WeatherState.initial() =>
-//       WeatherState._(_factory.first(_WeatherInitial()));
-
-//   factory WeatherState.loading() =>
-//       WeatherState._(_factory.second(_WeatherLoading()));
-
-//   factory WeatherState.loaded(int temperature) =>
-//       WeatherState._(_factory.third(_WeatherLoaded(temperature)));
-// }
-
 class CategorieState extends Union4Impl {
   CategorieState._(
       Union4<_CategorieInitial, _CategorieLoading, _CategorieSuccess,
@@ -28,21 +10,19 @@ class CategorieState extends Union4Impl {
       : super(union);
 
   static const Quartet<_CategorieInitial, _CategorieLoading, _CategorieSuccess,
-          _CategorieError> _factory =
-      Quartet<_CategorieInitial, _CategorieLoading, _CategorieSuccess,
-          _CategorieError>();
+      _CategorieError> _union = Quartet();
 
   factory CategorieState.initial() =>
-      CategorieState._(_factory.first(_CategorieInitial()));
+      CategorieState._(_union.first(_CategorieInitial()));
 
   factory CategorieState.loading() =>
-      CategorieState._(_factory.second(_CategorieLoading()));
+      CategorieState._(_union.second(_CategorieLoading()));
 
   factory CategorieState.sucess() =>
-      CategorieState._(_factory.third(_CategorieSuccess()));
+      CategorieState._(_union.third(_CategorieSuccess()));
 
   factory CategorieState.error() =>
-      CategorieState._(_factory.fourth(_CategorieError()));
+      CategorieState._(_union.fourth(_CategorieError()));
 }
 
 class _CategorieInitial {}
